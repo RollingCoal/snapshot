47b8432175849bbc45f1ae1b47490d558a6d17a1
jsayre-local <jsayre-local@17-1266.hsd1.in.comcast.net>
2020-06-18 13:39:09 -0400

    updated with new features for snapshots, starting, stopping

---
+++ shotty/shotty.py
@@ -0,0 +1,162 @@
+### Importing modules required for script
+import boto3
+import click
+
+### Global resources
+session = boto3.Session(profile_name='snapshot')
+ec2 = session.resource('ec2')
+
+### Function calls instances in AWS
+def filter_instances(project):
+    instances = []
+
+    if project:
+        filters = [{'Name':'tag:Project', 'Values':[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    return instances
+
+
+@click.group()
+def cli():
+    """Shotty Manages snapshots"""
+
+### SLI Group for Snapshots
+@cli.group('snapshots')
+def snapshots():
+    """Commands for snapshots"""
+
+@snapshots.command('list')
+@click.option('--project', default=None,
+    help="Only snapshots for project (tag Project:<name>)")
+def list_volumes(project):
+    "List EC2 snapshots"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+            for v in i.volumes.all():
+                for s in v.snapshots.all():
+                    print(", ".join((
+                        s.id,
+                        v.id,
+                        i.id,
+                        s.state,
+                        s.progress,
+                        s.start_time.strftime("%c")
+                    )))
+    return
+
+### CLI group for listing volumes in AWS
+@cli.group('volumes')
+def volumes():
+    """Commands for volumes"""
+
+@volumes.command('list')
+@click.option('--project', default=None,
+    help="Only volumes for project (tag Project:<name>)")
+def list_voumes(project):
+    "List EC2 volumes"
+
+    instances = filter_instances(project)
+    for i in instances:
+        for v in i.volumes.all():
+            print(", ".join((
+                v.id,
+                i.id,
+                v.state,
+                str(v.size) + "GiB",
+                v.encrypted and "Encrypted" or "Not Encrypted"
+            )))
+
+    return
+
+### Cli group for pulling instance information from AWS
+@cli.group('instances')
+def instances():
+    """Commands for instnaces"""
+
+@instances.command('snapshot',
+    help="Create snapshots of all volumes")
+@click.option('--project', default=None,
+    help="Only instances for project (tag Project:<name>)")
+def create_snapshots(project):
+    "Create snapshots for EC2 instances"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        print("Stopping {0}...".format(i.id))
+
+        i.stop()
+        i.wait_until_stopped()
+
+        for v in i.volumes.all():
+            print(" Creating snapshot of {0}".format(v.id))
+            v.create_snapshot(Description="Created by SnapshotAlyzer")
+
+        print("Starting {0}...".format(i.id))
+
+        i.start()
+        i.wait_until_running()
+
+    print("Job's done!")
+
+    return
+
+###### lists instances
+@instances.command('list')
+@click.option('--project', default=None,
+    help="Only instances for project (tag Project:<name>)")
+def list_instances(project):
+    "List EC2 instances"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        tags = { t['Key']: t['Value'] for t in i.tags or [] }
+        print(', '.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name,
+            tags.get('Project', '<no project>')
+            )))
+
+    return
+
+###### Stops instances
+@instances.command('stop')
+@click.option('--project', default=None,
+    help="Only instances for project (tag Project:<name>)")
+def stop_instances(project):
+    "Stop EC2 instances"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        print("Stopping {0}...".format(i.id))
+        i.stop()
+
+    return
+
+###### start instances
+@instances.command('start')
+@click.option('--project', default=None,
+    help="Only instances for project (tag Project:<name>)")
+def start_instances(project):
+    "Start EC2 instances"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        print("Starting {0}...".format(i.id))
+        i.stop()
+
+    return
+
+if __name__ == '__main__':
+    cli()
